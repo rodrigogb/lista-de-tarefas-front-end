909813f776850cc9f82f55dab884f10e69bf31a7
Rodrigo GB <rodrigogbuzato@hotmail.com>
2020-07-16 20:09:06 -0300

    Usando Router para redirecionar do login para a welcome page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { WelcomeComponent } from '../welcome/welcome.component';
 
 @Component({
   selector: 'app-login',
@@ -11,8 +13,15 @@ export class LoginComponent implements OnInit {
   password = ''
   errorMessage = 'Invalid Credentials'
   invalidLogin = false;
+  
 
-  constructor() { }
+  // Router
+  // Angular.giveMeRouter
+  // Dependency Injection
+  
+  constructor(private router: Router) { 
+  
+  }
 
   ngOnInit() {
   }
@@ -21,8 +30,11 @@ export class LoginComponent implements OnInit {
     console.log(this.username);
     if(this.username !== 'rodrigo' || this.password !== '123') {
       this.invalidLogin = true;
+
     } else {
       this.invalidLogin = false;
+      // Redirect to WelcomePage
+      this.router.navigate(['welcome'])
     }
   }
 
