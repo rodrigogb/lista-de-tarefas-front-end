27adbe4a1ca6b21fac30b0375e1573dd7de6011f
Rodrigo GB <rodrigogbuzato@hotmail.com>
2020-07-18 16:44:28 -0300

    Implementando o route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
 import { LogoutComponent } from './logout/logout.component';
+import { RouteGuardService } from './service/route-guard.service';
 
 // welcome
 const routes: Routes = [
@@ -12,9 +13,9 @@ const routes: Routes = [
   // caso nao exista o path (**) o app redireciona pro ErrorComponent
   { path:'', component: LoginComponent },
   { path:'login', component: LoginComponent },
-  { path:'welcome/:name', component: WelcomeComponent },
-  { path:'todos', component: ListTodosComponent },
-  { path:'logout', component: LogoutComponent },
+  { path:'welcome/:name', component: WelcomeComponent, canActivate:[RouteGuardService] },
+  { path:'todos', component: ListTodosComponent, canActivate:[RouteGuardService] },
+  { path:'logout', component: LogoutComponent, canActivate:[RouteGuardService] },
   { path:'**', component: ErrorComponent } 
 ];
 
