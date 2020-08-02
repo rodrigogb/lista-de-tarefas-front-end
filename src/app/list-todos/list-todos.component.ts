16f001e74a5b6e0f83746aa7269173b26a6927ff
Rodrigo Buzato <rodrigo.buzato@ventron.com.br>
2020-08-02 15:11:12 -0300

    Criado serviço para deletar.

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from '../service/data/todo-data.service';
 
 export class Todo {
   constructor(
     public id: number,
     public description: string,
-    public done: boolean,
+    public isDone: boolean,
     public targetDate: Date
   ) {
 
@@ -18,15 +19,41 @@ export class Todo {
 })
 export class ListTodosComponent implements OnInit {
 
-  todos = [    
-    new Todo(1,'Learn to Dance', false, new Date()),
-    new Todo(2,'Become an Expert in Angular', false, new Date()),
-    new Todo(3,'Learn to Fly', false, new Date()),
-    new Todo(4,'Learn to Swin', false, new Date()),
-  ]
-  constructor() { }
+  todos: Todo[]
+  message: string
+
+  // todos = [    
+  //   new Todo(1,'Learn to Dance', false, new Date()),
+  //   new Todo(2,'Become an Expert in Angular', false, new Date()),
+  //   new Todo(3,'Learn to Fly', false, new Date()),
+  //   new Todo(4,'Learn to Swin', false, new Date()),
+  // ]
+  constructor(
+    private todoService: TodoDataService
+  ) { }
 
   ngOnInit() {
+    this.refreshTodos();
+  }
+
+  refreshTodos() {
+    this.todoService.retrieveAllTodos('rodrigo').subscribe(
+      response => {
+        console.log(response);
+        this.todos = response;
+      }
+    )
+  }
+
+  deleteTodo(id) {
+    console.log(`delete todo ${id}`)
+    this.todoService.deleteTodo('in28minutes', id).subscribe(
+      response => {
+        console.log(response);
+        this.message = `Delete of Todo ${id} successful!`;
+        this.refreshTodos();
+      }
+    )
   }
 
 }
---
+++ src/app/service/data/todo-data.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Todo } from 'src/app/list-todos/list-todos.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoDataService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  retrieveAllTodos(userName) {
+    return this.http.get<Todo[]>(`http://localhost:8090/users/${userName}/todos`);
+ 
+   }
+
+   deleteTodo(userName, id) {
+     return this.http.delete(`http://localhost:8090/users/${userName}/todos/${id}`)
+   }
+
+}
