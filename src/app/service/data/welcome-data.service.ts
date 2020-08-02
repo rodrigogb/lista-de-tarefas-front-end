6d8d6f57863f9d454e541929e25185c4ab4ffb85
Rodrigo Buzato <rodrigo.buzato@ventron.com.br>
2020-08-02 12:25:11 -0300

    Chamada do Hello Word do front para o back.

--- src/app/service/data/welcome-data.service.ts
+++ src/app/service/data/welcome-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+// define o formato da mensagem (retorno do servico)
 export class HelloWorldBean {
   constructor(public message: string) {}
 }
@@ -10,11 +11,18 @@ export class HelloWorldBean {
 })
 export class WelcomeDataService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient  
+    ) { }
 
   executeHelloWorldBeanService() {
-   return this.http.get<HelloWorldBean>('http://localhost:8090//hello-world-bean');
+   return this.http.get<HelloWorldBean>('http://localhost:8090/hello-world-bean');
 
   }
+
+  executeHelloWorldBeanServiceWithPathVariable(name) {
+    return this.http.get<HelloWorldBean>(`http://localhost:8090/hello-world/path-variable/${name}`);
+ 
+   }
 
 }
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -25,14 +25,39 @@ export class WelcomeComponent implements OnInit {
   }
 
   getWelcomeMessage() {
+    // se não chamar o subscribe() a chamada não vai aparecer no 'Network' 
+    // do browser e o serviço vai retornar apenas um observable.
+    // subscribe() é assicrono
     console.log(this.service.executeHelloWorldBeanService().subscribe(
-      response => this.handleSuccessfulResponse(response)
+      response => this.handleSuccessfulResponse(response),       
+      error => this.handleErrorResponse(error)
     ));
+
+    // pode ser exibido antes, pois é assincrono:
+    console.log('last line of getWelcomeMessage')
+
+  }
+
+  getWelcomeMessageWithParameter() {
+    // se não chamar o subscribe() a chamada não vai aparecer no 'Network' 
+    // do browser e o serviço vai retornar apenas um observable.
+    // subscribe() é assicrono
+    console.log(this.service.executeHelloWorldBeanServiceWithPathVariable(this.name).subscribe(
+      response => this.handleSuccessfulResponse(response),       
+      error => this.handleErrorResponse(error)
+    ));
+
+    // pode ser exibido antes, pois é assincrono:
+    console.log('last line of getWelcomeMessage')
 
   }
 
   handleSuccessfulResponse(response) {
     this.welcomeMessageFromService = response.message;
+  }
+
+  handleErrorResponse(error) {
+    this.welcomeMessageFromService = error.error.message;    
   }
 
 }
